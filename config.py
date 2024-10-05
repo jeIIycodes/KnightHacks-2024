694c968508779081a107ce5ef2ec543b27c09ba5
michaelbennie <michaelbennie@ufl.edu>
2024-10-04 22:34:23 -0400

    Setup base scrute using Kinde auth

---
+++ config.py
@@ -0,0 +1,20 @@
+from kinde_sdk.kinde_api_client import GrantType
+
+
+SITE_HOST = "localhost"
+SITE_PORT = "5000"
+SITE_URL = f"http://{SITE_HOST}:{SITE_PORT}"
+GRANT_TYPE = GrantType.AUTHORIZATION_CODE_WITH_PKCE
+CODE_VERIFIER = "joasd923nsad09823noaguesr9u3qtewrnaio90eutgersgdsfg" # A suitably long string > 43 chars
+TEMPLATES_AUTO_RELOAD = True
+SESSION_TYPE = "filesystem"
+SESSION_PERMANENT = False
+SECRET_KEY = "joasd923nsad09823noaguesr9u3qtewrnaio90eutgersgdsfgs" # Secret used for session management
+MGMT_API_CLIENT_ID=""
+MGMT_API_CLIENT_SECRET=""
+
+KINDE_ISSUER_URL = "https://2024knighthacks.kinde.com"
+KINDE_CALLBACK_URL = "http://localhost:5000/api/auth/kinde_callback"
+LOGOUT_REDIRECT_URL = "http://localhost:5000"
+CLIENT_ID = "1e6a29e092834b2183e0b531b2de63b5"
+CLIENT_SECRET = "7IFpdQbbyvA5Eyd9BzlsWenbyiXVa1ZkFdizdNQjEmHZcP5cJaSq"
