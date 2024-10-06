4436528d7c2c202c42735c6f21ac25e1d1145cf3
michaelbennie <michaelbennie@ufl.edu>
2024-10-06 02:57:57 -0400

    ai appi server is up

--- AiPredictor/APISERVER.py
+++ AiPredictor/APISERVER.py
@@ -1,15 +1,17 @@
 # app.py
 
 from flask import Flask
-from AiPredictor.config import Config
+from config import Config
 from blueprints.routes import database_bp
+from blueprints.recommendations import recommendations_bp  # Import the new blueprint
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     # Register blueprints
-    app.register_blueprint(database_bp)
+    app.register_blueprint(database_bp)            # Existing Blueprint
+    app.register_blueprint(recommendations_bp, url_prefix='/recommend')  # New Blueprint
 
     return app
 
