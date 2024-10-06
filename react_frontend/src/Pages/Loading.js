c287acbe86da24529cdebb7a6c38ac40057220ab
JoJo Pose <joshuai.paz@gmail.com>
2024-10-06 06:13:35 -0400

    Update Loading.js
    Copied Loading Page Code

--- react_frontend/src/Pages/Loading.js
+++ react_frontend/src/Pages/Loading.js
@@ -1,1 +1,18 @@
 //Loading.js
+import React from 'react';
+import './App.css'; // Could give issues if the name is not changed to "./loading.css"
+
+function App() {
+  return (
+    <div className="loading-screen">
+      <div className="loading-text">
+        Here Are Your Potential Candidates
+      </div>
+      <div className="swipe-instructions">
+        <p>To accept, click right or the <span style={{ color: 'red' }}>♥</span></p>
+        <p>To reject, click left or the <span style={{ color: 'red' }}>✘</span></p>      </div>
+    </div>
+  );
+}
+
+export default App;
